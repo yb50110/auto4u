0fb28b91d0e34c4c368c67a1ae86bd937af5a902
yunhaseo <yunha@b302.nl>
2017-11-26 15:28:43 -0600

    Login page

--- login.php
+++ login.php
@@ -5,19 +5,20 @@
     session_start();
     if($_SERVER["REQUEST_METHOD"] == "POST")
     {
-    // username and password sent from Form
-        $myusername=mysqli_real_escape_string($db,$_POST['username']);
-        $mypassword=mysqli_real_escape_string($db,$_POST['password']);
-        $sql="SELECT * FROM admin WHERE username='$myusername' and password='$mypassword'";
-        $result=mysqli_query($db,$sql);
+        // username and password sent from Form
+        $myusername=mysqli_real_escape_string($dbhandle,$_POST['login_username']);
+        $mypassword=mysqli_real_escape_string($dbhandle,$_POST['login_password']);
+
+        $sql=" SELECT * FROM login WHERE login_username='$myusername' and login_password='$mypassword' ";
+        $result=mysqli_query($dbhandle,$sql);
         $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
 
         $count=mysqli_num_rows($result);
 
-    // If result matched $myusername and $mypassword, table row must be 1 row
+        // If result matched $myusername and $mypassword, table row must be 1 row
         if($count==1)
         {
-            $_SESSION["username"] = $myusername;
+            $_SESSION["login_username"] = $myusername;
             header("location: index.php");
         }
         else
@@ -46,10 +47,10 @@
             </div>
 
             <div class="panel-body">
-                <div style = "font-size:0.75em; color:#cc0000; margin:10px 0 20px 0;"><?php echo $error; ?></div>
-                <form action = "" method = "post">
-                    <input type = "text" name = "username" class = "box" placeholder="username"/><br /><br />
-                    <input type = "password" name = "password" class = "box" placeholder="password"/><br/><br />
+                <div style="font-size:0.75em; color:#cc0000; margin:10px 0 20px 0;"><?php echo $error; ?></div>
+                <form action="" method="post">
+                    <input type="text" name="login_username" class="box" placeholder="username"/><br /><br />
+                    <input type="password" name="login_password" class="box" placeholder="password"/><br/><br />
                     <button type = "submit">Submit</button>
                 </form>
             </div>
